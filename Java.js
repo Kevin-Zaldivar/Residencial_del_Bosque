4ba262f6124374f8cc0167e4b8203dd739f7c7c7
Kevin-Zaldivar <124185857+Kevin-Zaldivar@users.noreply.github.com>
2025-06-17 21:04:39 -0600

    Correccion 3

--- Java.js
+++ Java.js
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 function iralPlano() {
     // Cambia la URL según tu necesidad
     window.location.href = "https://residencialdelbosque.gosoft.app//mapa#:~:text=Urbanizaci%C3%B3n-,Abierta";
-=======
-function iralPlano() {
-    // Cambia la URL según tu necesidad
-    window.location.href = "https://residencialdelbosque.gosoft.app//mapa#:~:text=Urbanizaci%C3%B3n-,Abierta";
->>>>>>> 7aaaf74466dd9682389f8c61ee1ffee99c28768c
 }
